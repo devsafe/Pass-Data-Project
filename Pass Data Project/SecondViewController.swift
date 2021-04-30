95d2beccbf37c8441f50bd3992176eea1a91dcf3
Rafinad <rafinad@outlook.com>
2021-04-30 23:58:18 +0300

    add new segue

---
+++ Pass Data Project/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Pass Data Project
+//
+//  Created by Сахар Кубический on 30.04.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    var login: String?
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)"
+    }
+    
+    @IBAction func goBackTap(_ sender: UIButton) {
+    }
+    
+
+}
--- Pass Data Project/ViewController.swift
+++ Pass Data Project/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+
+    @IBAction func loginTap(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
     }
-
-
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+    }
 }
 
